7ce7703fc270998f1b91415e5eb3620d95725c44
Matchobas <maalexandria@hotmail.com>
2022-05-01 00:13:32 -0300

    Multiple 3 and 5 challenge - finished

--- multiple3and5.js
+++ multiple3and5.js
@@ -1,7 +1,15 @@
 function solution(number){
-  if (number < 0) {
-    return 0;
+  let soma = 0;
+
+  for(var i = 1; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      soma += i;
+    }
   }
 
-  
+  return soma;
 }
+
+console.log(solution(10));
+console.log(solution(0));
+console.log(solution(-10));
