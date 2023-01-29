b24869eb85659b2c581e7deed5258d8cce95e683
Matchobas <maalexandria@hotmail.com>
2023-01-28 23:55:45 -0300

    Highest Scoring Word Kata kyu 6 final

--- highestScoringWord.ts
+++ highestScoringWord.ts
@@ -1,16 +1,22 @@
 let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 export const high = (str: string): string => {
-  let highestScoreWord = '';
-  str.split(" ").forEach((word) => {
+  const higher = str.split(" ").reduce((prev, word) => {
     const points = word.split('')
       .map((letter) => { return alphabet.findIndex((a) => a === letter) + 1 })
       .reduce((prev, cur) => { return prev + cur }, 0);
     
-      console.log(points);
-  });
+      if (points > prev.value) {
+        return {
+          value: points,
+          highestScoreWord: word
+        }
+      }
 
-  return '';
+      return prev;
+  }, { value: 0, highestScoreWord: ''});
+
+  return higher.highestScoreWord;
 }
 
-high("aabb aaaa x")
+console.log(high("nessa frase qual sera a maior das pontuacoes"));
