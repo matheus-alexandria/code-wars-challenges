1b13dd2b5acb4a42acefbb96015032c6d2a74c30
Matchobas <maalexandria@hotmail.com>
2023-01-29 23:49:20 -0300

    Stop spinning my words Kata kyu 6 first solution

--- stopSpinningMyWords.ts
+++ stopSpinningMyWords.ts
@@ -1,7 +1,22 @@
-function reverseWord(word: string) {
-  
+function reverseWord(word: string): string {
+  const wordLetters = word.split("");
+  const reverseWord = [];
+  while (wordLetters.length > 0) {
+    reverseWord.push(wordLetters.pop());
+  }
+
+  return reverseWord.join("");
 }
 
 export function spinWords(words: string): string {
-  return '';
+  return words.split(" ")
+    .map((word) => {
+      if (word.split("").length >= 5) {
+        return reverseWord(word);
+      }
+      return word;
+    })
+    .join(" ");
 }
+
+console.log(spinWords("Stop spinning my words"));
