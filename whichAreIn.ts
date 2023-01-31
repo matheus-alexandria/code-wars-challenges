593be083a6065720ed6ff549865ac7e45baef6e2
Matchobas <maalexandria@hotmail.com>
2023-01-30 22:30:50 -0300

    Which are in Kata 6 kyu codewars solution

--- whichAreIn.ts
+++ whichAreIn.ts
@@ -12,5 +12,12 @@ export function inArray(references: string[], words: string[]): string[] {
   return [...new Set(result)].sort();
 }
 
+// Solution from codewars
+export function inArrayFromKata(a1: string[], a2: string[]): string[] {
+  return [...new Set(a1.filter((str) => a2.some((str2) => str2.includes(str))).sort())];
+}
+
 console.log(inArray(["arp", "live", "strong"], ["lively", "alive", "harp", "sharp", "armstrong"]));
 console.log(inArray(["tarp", "mice", "bull"], ["lively", "alive", "harp", "sharp", "armstrong"]));
+
+console.log(inArrayFromKata(["tarp", "live", "strong"], ["lively", "alive", "harp", "sharp", "armstrong"]));
